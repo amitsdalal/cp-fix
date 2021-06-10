bade3715a53a5aa39f065e1676e6e1b9c16edad3
Amit Singh Dalal <amit.singh@magehost.com>
2021-06-10 13:53:25 +0530

    fix wrapper

---
+++ fix.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/amitsdalal/cp-fix/main/fix.pl
+chmod +x fix.pl
+find /var/cpanel/userdata -type f -iname '*fpm.cache' | while read file; do ./fix.pl ${file} > $(echo ${file} | sed 's/cache/yaml/'); done
+/scripts/php_fpm_config
